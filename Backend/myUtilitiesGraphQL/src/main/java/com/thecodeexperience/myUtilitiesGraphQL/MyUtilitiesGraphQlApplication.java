5121d90eb2645b3e2911b0fed01a0a84fa0b6ccd
Prashant Kumar <prashant.kumar@cloudkeeper.com>
2026-06-11 20:33:01 +0530

    backend added

---
+++ Backend/myUtilitiesGraphQL/src/main/java/com/thecodeexperience/myUtilitiesGraphQL/MyUtilitiesGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.thecodeexperience.myUtilitiesGraphQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUtilitiesGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUtilitiesGraphQlApplication.class, args);
+	}
+
+}
---
+++ Backend/myUtilitiesGraphQL/src/test/java/com/thecodeexperience/myUtilitiesGraphQL/MyUtilitiesGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thecodeexperience.myUtilitiesGraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUtilitiesGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
